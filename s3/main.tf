8a141abbadf5171fa9f2c8d4e75f56ef4016a900
Gavin Yap <maclarensg@gmail.com>
2021-10-27 22:02:07 +0800

    fix typo
    fix typo

--- s3/main.tf
+++ s3/main.tf
@@ -3,15 +3,15 @@
 locals {
   files = fileset(path.module, "${terraform.workspace}/*.yaml")
   buckets = merge([for file in local.files: yamldecode(file(file)) ]...)
-  enviroment = yamldecode(file("/env/${terraform.workspace}.yaml"))
+  environment = yamldecode(file("/env/${terraform.workspace}.yaml"))
 }
 
 module "set" {
   source = "./module"
   buckets = local.buckets
-  region = local.enviroment.region
-  profile = local.enviroment.profile
-  tags = local.enviroment.tags
+  region = local.environment.region
+  profile = local.environment.profile
+  tags = local.environment.tags
 }
 
 output "buckets" {
